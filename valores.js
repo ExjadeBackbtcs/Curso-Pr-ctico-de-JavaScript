a399abc9997581569763e3e002bc7d0cdb838fb1
Adrian Zamora <adrianzamora10@outlook.es>
2021-08-15 20:41:20 -0600

    Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis

---
+++ valores.js
@@ -0,0 +1,31 @@
+
+// Valores básicos / primitivos
+
+/* 
+     Javascript no entenderá  40 o Adrian Zamora como texto.  Debemos ponerlo en un String 
+*/
+"Texto Guardado" 
+
+/*  Jorge Boolean - Valores de Verdadero o Falso para validar sí tomamos una decisión o no.
+*/
+
+true
+false
+
+/*  Emptu values
+
+    Se les conoce como valores placeHolder
+
+    Cuando queda un valor faltante, guardamos ese valor en memoria
+*/
+null
+undefined
+
+
+// valores tipo objetos 
+
+/* 
+    Arrays y Objetos
+*/
+[1,2,3]
+{ nombre: "Diego" }
